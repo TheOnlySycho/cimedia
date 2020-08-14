db01df158e71f092fcfc8701d096765ddce53fb1
JuiciiYT <56958664+JuiciiYT@users.noreply.github.com>
2020-08-14 15:16:09 +0100

    Create boot.js

---
+++ boot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Boot is starting!');
+  client.user.setActivity("Boot Up…", {
+  type: "PLAYING"
+});
+client.user.setStatus(`dnd`)
+client.login('NzQwNTI4NTA2MTY0NzQwMTc3.XyqU6g.1Xodjmlt-9sLCVJn5JnipQ7CCCw');
