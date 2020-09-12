f950854dbcbcbb26ba3074bf820f80e397cf063c
JuiciiYT <56958664+JuiciiYT@users.noreply.github.com>
2020-09-12 19:24:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 //prefix
 const prefix = "c!"
-//Change to Rebootingslyinger
+//Change to Rebootingslyingerno
 
 //bot init
 client.once('ready', () => {
